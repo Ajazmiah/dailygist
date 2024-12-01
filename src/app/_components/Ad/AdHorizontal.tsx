ffcf19ef9726c4737ecce5fd9f5d036978aa35b7
Ajaz Miah <ajazmiah@yahoo.com>
2024-11-30 19:26:41 -0500

    horizontal ad

--- src/app/_components/Ad/AdHorizontal.tsx
+++ src/app/_components/Ad/AdHorizontal.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
 
 function AdHorizontal() {
+  useEffect(() => {
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {}
+      );
+    } catch (err) {}
+  }, []);
+
   return (
-    <div className='w-full bg-gray-700  h-[300px] mt-[2em] mb-[2em]'>ad Horizontal</div>
-  )
+    <div className="w-full bg-gray-700  h-[300px] mt-[2em] mb-[2em]">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-4339219773762700"
+        data-ad-slot="4484528772"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
+  );
 }
 
-export default AdHorizontal
+export default AdHorizontal;
